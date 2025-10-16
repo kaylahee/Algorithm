0bb3c8a19c54e6ffd424c5afb47b4e2f8b01241b
Seunghui Han <92175769+kaylahee@users.noreply.github.com>
2025-10-17 00:12:43 +0900

    Clean up code by removing unused functions
    Removed unused compare function and commented-out code.

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -7,10 +7,6 @@ using namespace std;
 
 int range[3000];
 
-bool compare(std::pair<int, int> a, std::pair<int, int> b){
-    return a.second < b.second;
-}
-
 int changeToint(std::string t, bool isEnd){
     std::string hour;
     for (int i = 0; i < 2; i++){
@@ -50,14 +46,10 @@ int solution(vector<vector<string>> book_time) {
         int st_t = changeToint(st, false);
         int en_t = changeToint(en, true);
         
-        // Time_val.push_back({st_t, en_t});
-        
         for (int j = st_t; j < en_t; j++){
             range[j]++;
         }
     }
-    
-    // std::sort(Time_val.begin(), Time_val.end(), compare);
     
     int maxCnt = 0;
     for (int i = 0; i < 1450; i++){
@@ -68,21 +60,6 @@ int solution(vector<vector<string>> book_time) {
     }
     
     int answer = maxCnt;
-    
-//     int en_idx = -1;
-//     for (std::pair<int, int> t : Time_val){
-//         int st = t.first;
-//         int en = t.second;
-        
-//         std::cout << st << " " << en << "\n";
-        
-//         if (st >= en_idx){
-//             answer++;
-//             en_idx = en;
-//         }
-//     }    
-    
-//     std::cout << answer << "\n";
     
     return answer;
 }
