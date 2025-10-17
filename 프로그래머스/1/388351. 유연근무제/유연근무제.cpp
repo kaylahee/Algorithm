9b5e254868bd7afe75d269c725a43b2d752bdff5
Seunghui Han <92175769+kaylahee@users.noreply.github.com>
2025-10-17 09:08:20 +0900

    [level 1] Title: 유연근무제, Time: 2.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
@@ -1,50 +1,53 @@
-// 23:34~
-// [프로그래머스] 유연근무제
+// 출근 희망 시각 + 10분까지 어플로 출근해야 함
+// 토요일, 일요일 출근 시각은 이벤트에 영향 끼치지 않음
+// 모든 시각은 시에 100을 곱하고 분을 더한 정수로 표현됨
 
-// 출근 희망 시각에 늦지 않고 출근한 직원들에게 상품을 주는 이베트
-// 일주일 동안 자신이 설정한 출근 희망 시각 + 10분까지 출근해야 함
-// 토요일, 일요일의 출근 시각은 이벤트에 영향 X
-// 시 * 100 + 분
+// 상품 받을 직원은 몇명?
 
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) {
-    int cnt = 0;
-    for (int people = 0; people < timelogs.size(); people++){
-        int m = schedules[people] % 100;
-        int h = (schedules[people] - m) / 100;
-        m += 10;
+    int answer = 0;
+    for (int i = 0; i < schedules.size(); i++){
+        int goal_t = schedules[i] + 10;
         
+        int h = goal_t / 100;
+        int m = goal_t % 100;
         if (m >= 60){
             m -= 60;
+            
             h += 1;
             if (h == 24){
-                h = 0;
+                h -= 24;
             }
         }
-        int st = h * 100 + m;
         
-        bool flag = true;
+        goal_t = h * 100 + m;
+        // std::cout << goal_t << " ";
+        
+        int cnt = 0;
         int day = 0;
-        for (int i = startday; i < 7 + startday; i++){        
-            int day_idx = (i - 1) % 7;
-            // 주말에는 이벤트 X
-            if (day_idx == 5 || day_idx == 6){
-                day++;
+        for (int j = 0 + startday; j < 7 + startday; j++){
+            day++;
+            if (j % 7 == 6 || j % 7 == 0){
                 continue;
             }
             
-            if (timelogs[people][day] > st){
-                flag = false;
+            std::cout << j % 7 << " " << goal_t << " " << timelogs[i][day] << "\n";
+            if (goal_t < timelogs[i][day - 1]){
+                continue;
             }
-            day++;
-        }   
-        if (flag){
+            
             cnt++;
         }
-    }    
-    return cnt;
+        
+        if (cnt == 5){
+            answer++;
+        }
+    }
+    return answer;
 }
